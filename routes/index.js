441df96d3b259f67c2a90292e182a2e7f8b86879
Marc Augier <m.augier@me.com>
2022-11-22 02:01:42 +0100

    la page de garde affiche la liste des clients

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Les routes par page
-const clientsRoute = require('./clients');
-const contactRoute = require('./contact');
+const clientsRoute = require("./clients");
+const contactRoute = require("./contact");
 
-module.exports = params => {
-    
-  router.get('/', (request, response) => {
+module.exports = (params) => {
+  /*   router.get('/', (request, response) => {
     response.render('layout', { pageTitle: 'Project Planner', template: 'index' });
-  });
+  }); */
 
-  router.use('/client', clientsRoute(params));
-  router.use('/contact', contactRoute(params));
+  router.use("/", clientsRoute(params));
+
+  router.use("/client", clientsRoute(params));
+  router.use("/contact", contactRoute(params));
 
   return router;
 };
-  
