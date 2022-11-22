964a8c6d0c5acc4955c420bf089a85d58397084e
Marc Augier <m.augier@me.com>
2022-11-22 22:19:12 +0100

    adding planning and preparing task modification

---
+++ models/Planning.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const Planning = sequelize.define(
+    "Planning",
+    {
+      planningId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      taskId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      }
+    },
+    {
+      // Ne pas ajouter les attibuts timestamps (updatedAt, createdAt)
+      timestamps: false,
+      // Pas de createdAt
+      createdAt: false,
+      // Pas de updatedAt
+      updatedAt: false,
+    }
+  );
+
+  return Planning;
+};
