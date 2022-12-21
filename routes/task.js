267a585e983199c89fee6af8f5107a3d06c92c0d
Marc Augier <m.augier@me.com>
2022-12-21 01:29:04 +0100

    review routing for tasks

--- routes/task.js
+++ routes/task.js
@@ -5,30 +5,28 @@ const router = express.Router();
 module.exports = params => {
     const { db, clientController, projectController, taskController } = params;
 
-    router.get('/', async (request, response) => {
-        let cmd = request.query.cmd;
-        const id = request.query.taskId;
-        console.log(id);
-        if (!id){
-            return response.redirect('/project');
-        }
-        const { task, project } = await taskController.getFullTask(id);
+    router.get('/:id', async (request, response) => {
+        const { task, project } = await taskController.getFullTask(request.params.id);
+        return response.render('layout', { pageTitle: 'Task Details', template: 'task_details', task, project });
+    });
 
-        switch(cmd){
+    router.get('/delete/:id', async (request, response) => {
+        return response.render('layout', { pageTitle: 'Task Delete', template: 'task_delete' });
+    });
+
+    router.get('/plan/:id/:step', async (request, response) => {
+        const step = request.params.step;
+
+        const { task, project } = await taskController.getFullTask(request.params.id);
+        return response.render('layout', { pageTitle: 'Task Creation', template: 'task_create', task, project });
+    });
+
+        /* TODO 
             case "details":
                 return response.render('layout', { pageTitle: 'Task Details', template: 'task_details', task, project });
-                break;
             case "edit":
                 return response.render('layout', { pageTitle: 'Task Modification', template: 'task_edit', task, project });
-                break;
-            case "delete":
-                return response.render('layout', { pageTitle: 'Task Delete', template: 'index' });
-                break;
-            default:
-                return response.redirect('/');
-                break;
-        }
-    });
+         */
 
    return router;
 };
