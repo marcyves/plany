12501cce9a5cb4f34cbd0ce3d4029ab0ea9afdaf
Marc Augier <m.augier@me.com>
2023-05-26 23:42:59 +0200

    consistent parameter SQL_LOGGING

--- config/db.config.js
+++ config/db.config.js
@@ -8,7 +8,7 @@ module.exports = {
   PASSWORD: process.env.DB_USER_PASSWORD,
   DATABASE_URL: process.env.DATABASE_URL,
   DB: process.env.DB_NAME,
-  LOG: process.env.SQL_LOGGING,
+  SQL_LOGGING: process.env.SQL_LOGGING,
   dialect: "mysql",
   pool: {
     max: 5,
