19a45a83aa10af09708c7d3f6b8d5019c3b601dc
Marc Augier <m.augier@me.com>
2023-05-26 23:24:43 +0200

    Adding support to PlanetScale

--- config/db.config.js
+++ config/db.config.js
@@ -2,9 +2,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
+  VERSION: process.env.VERSION,
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_USER_PASSWORD,
+  DATABASE_URL: process.env.DATABASE_URL,
   DB: process.env.DB_NAME,
   LOG: process.env.SQL_LOGGING,
   dialect: "mysql",
--- models/index.js
+++ models/index.js
@@ -1,28 +1,44 @@
 const config = require("../config/db.config.js");
-const {Sequelize, Op} = require("sequelize");
+const { Sequelize, Op } = require("sequelize");
 
-const logging = config.LOG === "true"?true:false;
-const production = config.VERSION === "PROD"?true:false;
+const logging = config.SQL_LOGGING === "true" ? true : false;
+const version = config.VERSION;
 
-if (production){
-  // MySQL Server Configuration
-  var database = new Sequelize(config.DB, config.USER, config.PASSWORD, 
-    {
-    host: config.HOST,
-    dialect: config.dialect,
-    logging: logging,                     // true pour avoir le détail des requêtes SQL
-    pool: {
-      max: config.pool.max,
-      min: config.pool.min,
-      acquire: config.pool.acquire,
-      idle: config.pool.idle,
-    },
-  }); 
-} else {
-  // SQLite plus facile pour les tests
-  //const database = new Sequelize('sqlite::memory', {logging: logging});
-  var database = new Sequelize(`sqlite:${config.DB}.sqlite`, {logging: logging});
+switch (version) {
+  case "planetscale":
+    var database = new Sequelize(config.DATABASE_URL, {
+      logging: logging,
+      dialectOptions: {
+        ssl: { rejectUnauthorized: true },
+      },
+    });
+    break;
+  case "prod":
+    // MySQL Server Configuration
+    var database = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+      host: config.HOST,
+      dialect: config.dialect,
+      logging: logging, // true pour avoir le détail des requêtes SQL
+      pool: {
+        max: config.pool.max,
+        min: config.pool.min,
+        acquire: config.pool.acquire,
+        idle: config.pool.idle,
+      },
+    });
+    break;
+  case "dev":
+    // SQLite plus facile pour les tests
+    //const database = new Sequelize('sqlite::memory', {logging: logging});
+    var database = new Sequelize(`sqlite:${config.DB}.sqlite`, {
+      logging: logging,
+    });
+    break;
+  default:
+    console.log(`Sorry, there is no version ${version}.`);
 }
+console.log(`== Connected to version: ${version}`);
+
 
 const db = {};
 
@@ -36,17 +52,29 @@ db.project = require("./Project.js")(database, Sequelize.DataTypes);
 db.task = require("./Task.js")(database, Sequelize.DataTypes);
 db.planning = require("./Planning.js")(database, Sequelize.DataTypes);
 
-// Ajout des relations
-db.client.belongsTo(db.user);
-db.user.hasMany(db.client, {foreignKey: 'userId'});
+  // Ajout des relations
+  if (version == "planetscale") {
 
-db.project.belongsTo(db.client);
-db.client.hasMany(db.project, {foreignKey: 'clientId'});
+  db.client.belongsTo(db.user, {constraints: false,});
+  db.project.belongsTo(db.client, {constraints: false,});
+  db.task.belongsTo(db.project, { foreignKey: "projectId", constraints: false,});
+  db.planning.belongsTo(db.task, { foreignKey: "taskId", constraints: false,});
 
-db.task.belongsTo(db.project, {foreignKey: 'projectId'});
-db.project.hasMany(db.task, {foreignKey: 'projectId'});
+  db.user.hasMany(db.client, { foreignKey: "userId", constraints: false, });
+  db.client.hasMany(db.project, { foreignKey: "clientId", constraints: false, });
+  db.project.hasMany(db.task, { foreignKey: "projectId", constraints: false, });
+  db.task.hasMany(db.planning, { foreignKey: "taskId", constraints: false, });
 
-db.planning.belongsTo(db.task, {foreignKey: 'taskId'});
-db.task.hasMany(db.planning, {foreignKey: 'taskId'});
+} else {
+  db.client.belongsTo(db.user);
+  db.project.belongsTo(db.client);
+  db.task.belongsTo(db.project, { foreignKey: "projectId" });
+  db.planning.belongsTo(db.task, { foreignKey: "taskId" });
+
+  db.user.hasMany(db.client, { foreignKey: "userId" });
+  db.client.hasMany(db.project, { foreignKey: "clientId" });
+  db.project.hasMany(db.task, { foreignKey: "projectId" });
+  db.task.hasMany(db.planning, { foreignKey: "taskId" });
+}
 
 module.exports = db;
